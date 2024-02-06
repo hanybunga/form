4645a78bc1be8bd93d6d6bee74367e0b4b3c3142
hanybunga <156047484+hanybunga@users.noreply.github.com>
2024-02-06 12:45:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+var nameError = document.getElementById('name-error');
+var phoneError = document.getElementById('phohe-error');
+var emailError = document.getElementById('email-error');
+var messageError = document.getElementById('message-error');
+var submitError = document.getElementById('submit-error');
+
+function validateName(){
+    var name = document.getElementById('contact-name').value;
+
+    if(name.length == 0){
+        nameError.innerHTML = 'Name is required'
+        return false;
+    }
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameError.innerHTML = 'Write full name';
+        return false;
+    }
+    nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+function validatePhone(){
+    var phone = document.getElementById('contact-phone').value;
+
+    if(phone.length == 0){
+        phoneError.innerHTML = 'Phone no is required';
+        return false;
+    }
+    if(phone.length !== 10){
+        phoneError.innerHTML = 'Phone no should be 10 digits';
+        return false;
+    }
+    if(!phone.match(/^[0-9]{10}$/)){
+        phoneError.innerHTML = 'Only digits please';
+    }
+
+    phoneError.innerHTML = '<i class="fas fa-check-circle"></i>';
+    return true;
+}
